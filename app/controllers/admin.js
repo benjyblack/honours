8f1211adf2aff389a2c9a71b86fca44d9901872e
BenBlack <benjyblack@gmail.com>
2014-03-03 11:48:39 -0500

    integrate mailchimp API

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -5,8 +5,11 @@ var mongoose = require('mongoose'),
     _ = require('lodash'),
     randomstring = require('randomstring'),
     User = mongoose.model('User'),
-    csv = require('csv');
+    csv = require('csv'),
+    mcapi = require('mailchimp-api');
 
+
+mc = new mcapi.Mailchimp('e60376a181c62f3ddcb244acfd840ae1-us3');
 
 /**
  * Import CSV
@@ -18,14 +21,14 @@ exports.import = function(req, res) {
     csv().from.path(req.files.file.path, { delimiter: ',', escape: '"' })
     .on('record', function(row,index){
         var i;
+        var user;
 
         // First row will be the headerline
         if (index === 0) {
             for (i = 0; i < row.length; i++) headerline.push(row[i]);
         }
         else {
-        
-            var user = new User();
+            user = new User();
             user.type = 'Student';
             user.provider = 'local';
             user.password = randomstring.generate();
@@ -40,25 +43,58 @@ exports.import = function(req, res) {
 
         var numRecords = rowCount - 1; // one of the rows is the headliner
         var recordsIteratedThrough = 0;
-        var failedUserCount = 0;
+        var successfulUsers = [];
+        var failedUsers = [];
 
         usersToAdd.forEach(function(user) {
+            // Save user to the database
             user.save(function(err) {
-                if (err && err.err) {
-                    failedUserCount++;
-                }
-
+                if (err && err.err) failedUsers.push(user);
+                else successfulUsers.push(user);
+                
                 recordsIteratedThrough++;
                 if (recordsIteratedThrough === numRecords) {
-                    if (failedUserCount === 0)
+                    // Subscribe successful users asynchronously
+                    // TODO: Handle errors in subscription
+                    subscribeUsersToList(successfulUsers, '59aa66bc5c');
+
+                    if (failedUsers.length === 0) {
                         res.jsonp(201, { message: 'Successfully added ' + numRecords + ' users' });
-                    else
-                        res.jsonp(500, { message: 'Failed to add ' + failedUserCount + '/' + numRecords + '  users' });
+                    }
+                    else {
+                        res.jsonp(500, { message: 'Failed to add ' +
+                            failedUsers.length + '/' + numRecords + '  users' });
+                    }
                 }
             });
         });
     })
     .on('error', function(error){
         res.jsonp(500, { message: 'Error parsing CSV file: ' + error.message });
+    });
+};
+
+var subscribeUsersToList = function(users, listId, callback) {
+
+    users.forEach(function(user) {
+        mc.lists.subscribe(
+            {
+                id: listId,
+                email:{
+                    email: user.email
+                },
+                merge_vars: {
+                    fname: user.firstName,
+                    lname: user.lastName,
+                    password: user.password
+                }
+            },
+            function(data) {
+                
+            },
+            function(error) {
+                if (error.error) {
+                }
+        });
     });
 };
