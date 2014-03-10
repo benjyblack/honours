95a78d42e99a9f77bd1ee52c336b85f831aa8540
BenBlack <benjyblack@gmail.com>
2014-03-09 22:16:19 -0400

    update List to follow design pattern (panels)

--- public/js/controllers/questions/list.js
+++ public/js/controllers/questions/list.js
@@ -1,6 +1,7 @@
 angular.module('mean.questions').controller('QuestionsListController', ['$scope', '$routeParams', 'Global', 'Questions', function ($scope, $routeParams, Global, Questions) {
     $scope.global = Global;
     $scope.type = $routeParams.type || 'professor';
+    $scope.header = $scope.type === 'professor' ? 'Professor\'s Questions' : 'Student\'s Questions';
 
     $scope.find = function() {
         Questions.query(function(questions) {
